ec04a7b353f4f95ec3a8fe2bdd5ec48a284ad72e
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-03-22 20:58:02 -0300

    feat: implement app error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { environment } from "./utils/env";
+import { appErrorHandler } from "./middlewares/app-error-handler";
 
 const port = environment.port || 3000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(appErrorHandler);
 
 app.listen(port, () => console.log("app started on", port));
