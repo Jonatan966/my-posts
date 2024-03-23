9b3be49f90212a5860b4fcbd0da274f994ac900a
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-03-23 14:50:55 -0300

    feat: add user not found error

--- src/modules/user/errors.ts
+++ src/modules/user/errors.ts
@@ -2,12 +2,18 @@ import { ErrorProps } from "../../utils/error";
 
 export enum UserModuleErrorType {
   UserAlreadyExists = "user_already_exists",
+  UserNotFound = "user_not_found",
 }
 
 export const UserModuleErrors: Record<UserModuleErrorType, ErrorProps> = {
   [UserModuleErrorType.UserAlreadyExists]: {
     name: UserModuleErrorType.UserAlreadyExists,
     message: "This username is already exists",
+    statusCode: 400,
+  },
+  [UserModuleErrorType.UserNotFound]: {
+    name: UserModuleErrorType.UserNotFound,
+    message: "User not found",
     statusCode: 400,
   },
 };
