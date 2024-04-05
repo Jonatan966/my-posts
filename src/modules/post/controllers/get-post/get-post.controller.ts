56a1fd3dc9e7cc0c2e54b7525f48e39924cd2ec7
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-04-05 00:31:28 -0300

    feat: add route to get post

---
+++ src/modules/post/controllers/get-post/get-post.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { z } from "zod";
+import { getPost } from "../../usecases/get-post/get-post.usecase";
+
+export const getPostController = async (
+  request: Request,
+  response: Response
+) => {
+  const paramsSchema = z.object({
+    post_id: z.string(),
+  });
+
+  const params = await paramsSchema.parseAsync(request.params);
+
+  const post = await getPost(params.post_id);
+
+  return response.json(post);
+};
--- src/modules/post/routes.ts
+++ src/modules/post/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { appErrorHandler } from "../../middlewares/app-error-handler";
 import { createPostController } from "./controllers/create-post/create-post.controller";
+import { getPostController } from "./controllers/get-post/get-post.controller";
 
 const postRouter = Router();
 
 postRouter.post("/", appErrorHandler(createPostController));
+postRouter.get("/:post_id", appErrorHandler(getPostController));
 
 export { postRouter };
